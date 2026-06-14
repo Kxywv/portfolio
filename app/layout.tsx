19c54a113d7759f5a8bf641cecb0056d111d408c
KEY WAV <muhammadpadhillah037@gmail.com>
2026-06-15 01:13:52 +0700

    fix: remove pnpm lockfiles and empty href icon entry

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,6 @@ export const metadata: Metadata = {
         url: '/icon-dark-32x32.png',
         media: '(prefers-color-scheme: dark)',
       },
-      {
-        url: '',
-        type: 'image/svg+xml',
-      },
     ],
     apple: '/apple-icon.png',
   },
